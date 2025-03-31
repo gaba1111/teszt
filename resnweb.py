dec938dff569a0cac1e58706259155e17e67e3e7
gaba1111 <59851274+gaba1111@users.noreply.github.com>
2025-03-31 12:46:46 +0200

    Update resnweb.py

--- resnweb.py
+++ resnweb.py
@@ -7,6 +7,7 @@ HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'hu-HU,hu;q=0.9',
     'priority': 'u=0, i',
+    'cookie': '',
     'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"macOS"',
