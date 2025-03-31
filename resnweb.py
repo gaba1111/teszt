ba5803258f3bdabfd06c70551d8cc3a1de6fc368
gaba1111 <59851274+gaba1111@users.noreply.github.com>
2025-03-31 12:22:36 +0200

    Update resnweb.py

--- resnweb.py
+++ resnweb.py
@@ -3,13 +3,36 @@ import codecs
 import re
 from requests import Session
 
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'hu-HU,hu;q=0.9',
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'iframe',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-site',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+}
+
 def get_price(hotel_config, arrive, departure):
     session = Session()
     requests_cfg = hotel_config["requests"]
 
     # REQUEST 1 - GET
-    response1 = session.get(requests_cfg[0]["url"])
-    sid = list(session.cookies)[0].value
+    response1 = session.get(requests_cfg[0]["url"], headers=HEADERS)
+    if response1.status_code != 200:
+        return f"Az 1. request sikertelen volt. HTTP státuszkód: {response1.status_code}"
+
+    if not session.cookies:
+        return "A szerver nem küldött vissza session cookie-t (sid). Ellenőrizd az 1. request URL-jét vagy fejléceit."
+
+    try:
+        sid = list(session.cookies)[0].value
+    except IndexError:
+        return "Nem sikerült lekérni a session ID-t (sid)."
 
     # REQUEST 2 - POST
     data2 = {
@@ -29,11 +52,11 @@ def get_price(hotel_config, arrive, departure):
     }
     data2.update(requests_cfg[1].get("params", {}))
 
-    session.post(requests_cfg[1]["url"], data=data2)
+    session.post(requests_cfg[1]["url"], headers=HEADERS, data=data2)
 
     # REQUEST 3 - GET
     url3 = requests_cfg[2]["url_template"].replace("{sid}", sid)
-    response3 = session.get(url3)
+    response3 = session.get(url3, headers=HEADERS)
 
     html = response3.text
     start_marker = "window.rooms"
@@ -95,7 +118,7 @@ def get_price(hotel_config, arrive, departure):
         }
         data4.update(requests_cfg[3].get("params", {}))
 
-        response4 = session.post(requests_cfg[3]["url"], data=data4)
+        response4 = session.post(requests_cfg[3]["url"], headers=HEADERS, data=data4)
         try:
             room_price = response4.json().get("price", {}).get("roomPrice", {}).get("hotelPrice")
             if room_price is not None:
