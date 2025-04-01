0fc621ed4b16f31eab2994a7fbb625078064b473
gaba1111 <59851274+gaba1111@users.noreply.github.com>
2025-04-01 10:45:39 +0200

    Update roomsome1.py

--- roomsome1.py
+++ roomsome1.py
@@ -1,35 +1,42 @@
 import json
 import re
 from requests import Session
-from json import JSONDecoder
-from urllib.parse import quote
+from urllib.parse import urlencode
+
 
 def get_price(hotel_config, arrive, departure):
     base_url = hotel_config["url"]
     session = Session()
 
     # --- Első request (POST) ---
-    payload = {
-        "room_persons[0][adult]": "2",
-        "room_persons[0][child]": "2",
-        "room_persons[0][child_ages][0]": "6",
-        "room_persons[0][child_ages][1]": "10",
+    payload_dict = {
+        "room_persons[0][adult]": 2,
+        "room_persons[0][child]": 2,
+        "room_persons[0][child_ages][0]": 6,
+        "room_persons[0][child_ages][1]": 10,
         "arrival": arrive,
         "departure": departure,
-        "rooms": "1",
+        "rooms": 1,
         "lang": "hu",
-        "subpage_num": "1",
-        "subpage_num_next": "2"
+        "subpage_num": 1,
+        "subpage_num_next": 2
     }
+
+    # Kézi URL-enkódolás
+    payload_encoded = urlencode(payload_dict)
 
     scraperapi_key = "c401c39c3f0f84264c25fb763591324f"
     post_url = f"{base_url}kereses"
-    encoded_post_url = quote(post_url, safe="")
-    full_post_url = f"http://api.scraperapi.com?api_key={scraperapi_key}&url={encoded_post_url}"
+    encoded_post_url = urlencode({"api_key": scraperapi_key, "url": post_url})
+    full_post_url = f"http://api.scraperapi.com?{encoded_post_url}"
 
-    response_post = session.post(full_post_url, data=payload, allow_redirects=True)
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response_post = session.post(full_post_url, data=payload_encoded, headers=headers, allow_redirects=True)
 
     if response_post.status_code != 200:
         return f"Az első POST kérés sikertelen volt. HTTP státuszkód: {response_post.status_code}"
 
-    return f"Első POST válasz első 1000 karaktere:\n{response_post.text[:1000]}"
+    return response_post.text[:1000]
